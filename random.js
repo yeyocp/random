7d387fe8ee1514c5eedf07dbb01bc7081bf46d48
Sergio Castaño <yeyo19833@hotmail.com>
2015-06-22 19:57:01 +0200

    random version 2

--- random.js
+++ random.js
@@ -1,9 +1,10 @@
 // Math.random() devuelve número aleatorio entre 0 y 1.
 var numero = Math.random();
 
+var str = ' MAYOR QUE 0,5';
+
 if(numero <= 0.5) {
-	console.log('\n' + numero + ' MENOR QUE 0,5 \n');
+	str = ' MENOR QUE 0,5';
 }
-else {
-	console.log('\n' + numero + ' MAYOR QUE 0,5 \n');
-}
+
+console.log('\n' + numero + str + '\n');
